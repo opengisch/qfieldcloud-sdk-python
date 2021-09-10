7c1aef33edb3fa201d630599c78b9003b2f8508f
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-09-10 19:15:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,170 @@
+# The official QFieldCloud SDK and CLI
+
+`qfieldcloud-sdk` is the official client to connect to QFieldCloud API either as a python module, or directly from the command line.
+
+## Install
+
+`pip install qfieldcloud-sdk`
+
+## Module usage
+
+```
+import qfieldcloud_sdk
+import requests
+
+client = qfieldcloud_sdk.Client(
+    url="https://app.qfield.cloud/api/v1/",
+    username="user1",
+    password="pass1",
+)
+
+try:
+    projects = client.projects()
+except requests.HttpRequest:
+    print("Oops!")
+```
+
+## CLI usage
+
+The official QFieldCloud CLI tool.
+
+### Usage
+
+```
+qfieldcloud-cli [OPTIONS] COMMAND [ARGS]...
+```
+
+### Examples
+
+```
+# logs in user "user" with password "pass"
+qfieldcloud-cli login user pass
+
+# gets the projects of user "user" with password "pass" at "https://localhost/api/v1/"
+qfieldcloud-cli -u user -p pass -U https://localhost/api/v1/ list-projects
+
+# gets the projects of user authenticated with token `QFIELDCLOUD_TOKEN` at "https://localhost/api/v1/" as JSON
+export QFIELDCLOUD_URL=https://localhost/api/v1/
+export QFIELDCLOUD_TOKEN=017478ee2464440cb8d3e98080df5e5a
+qfieldcloud-cli --json list-projects
+```
+
+### Global options overview
+
+```
+-U, --url TEXT       URL to the QFieldCloud API endpoint. If not passed,
+                    gets the value from QFIELDCLOUD_URL environment
+                    variable. Default: https://app.qfield.cloud/api/v1/
+
+-u, --username TEXT  Username or email.
+-p, --password TEXT
+-t, --token TEXT     Session token.
+--json / --human     Output the result as newline formatted json.
+--help               Show this message and exit.
+```
+
+Environment variables can be used instead of passing some common global options.
+
+- `QFIELDCLOUD_API` - QFieldCloud API endpoint URL
+- `QFIELDCLOUD_USERNAME` - QFieldCloud username or email. Need `QFIELDCLOUD_PASSWORD` to be set.
+- `QFIELDCLOUD_PASSWORD` - Password. Needs `QFIELDCLOUD_USERNAME` to be set.
+- `QFIELDCLOUD_TOKEN` - Token that can be used instead of passing username and password. It can be obtained by running `qfieldcloud-cli login`.
+
+### Commands overview
+
+```
+  download-files  Download QFieldCloud project files
+  list-files      List QFieldCloud project files
+  list-projects   List QFieldCloud projects
+  login           Login into QFieldCloud
+```
+
+#### login
+
+Login to QFieldCloud.
+
+```
+qfieldcloud-cli login [OPTIONS] USERNAME PASSWORD
+```
+
+#### list-projects
+
+List QFieldCloud projects.
+
+```
+qfieldcloud-cli list-projects [OPTIONS]
+
+Options:
+  --include-public / --no-public  Includes the public project in the list.
+```
+
+#### list-files
+
+List QFieldCloud project files.
+
+```
+qfieldcloud-cli list-files [OPTIONS] PROJECT_ID
+```
+
+#### download-files
+
+Download QFieldCloud project files.
+
+```
+qfieldcloud-cli download-files [OPTIONS] PROJECT_ID LOCAL_DIR
+
+Options:
+  --subdir TEXT                   Do not download the whole project, but only
+                                  the subdirectory passed.
+
+  --exit-on-error / --no-exit-on-error
+                                  If any project file download fails stop
+                                  downloading the rest.
+```
+
+## Development
+
+Contributions are more than welcome!
+
+### Code style
+Code style done with [precommit](https://pre-commit.com/).
+
+```
+pip install pre-commit
+# install pre-commit hook
+pre-commit install
+```
+
+### Cloning the project
+
+One time action to clone and setup:
+
+```
+git clone https://github.com/opengisch/qfieldcloud-sdk-python
+cd qfieldcloud-sdk-python
+# install dev dependencies
+python3 -m pip install --upgrade build pre-commit
+pre-commit install
+```
+
+To run CLI interface for development purposes execute:
+
+```
+python src/bin/qfieldcloud-cli
+```
+
+### Building the package
+
+Run:
+
+```
+python3 -m build
+```
+
+Then install on your system:
+
+```
+pip install dist/qfieldcloud_sdk-dev-py3-none-any.whl --force-reinstall
+```
+
+Voila!
---
+++ src/qfieldcloud_sdk/sdk.py
@@ -0,0 +1,166 @@
+import logging
+import os
+from enum import Enum
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+import requests
+
+
+class DownloadStatus(Enum):
+    PENDING = "PENDING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+
+
+class Client:
+    def __init__(self, url: Optional[str]) -> None:
+        """Prepares a new client. If the `url` is not provided, uses `QFIELDCLOUD_URL` from the environment."""
+        self.url = url or os.environ.get("QFIELDCLOUD_URL", "")
+        self.token = None
+
+        if not self.url:
+            raise Exception(
+                "Cannot create a new QFieldCloud client without a url passed in the constructor or as environment variable QFIELDCLOUD_URL"
+            )
+
+    def login(self, username: str, password: str) -> Dict:
+        """Logins with the provided credentials.
+
+        Args:
+            username: the username or the email used to register
+            password: the password associated with that username
+        """
+        resp = self._request(
+            "POST",
+            "auth/login",
+            data={
+                "username": username,
+                "password": password,
+            },
+        )
+
+        payload = resp.json()
+
+        self.token = payload["token"]
+
+        return payload
+
+    def list_projects(
+        self, username: Optional[str] = None, include_public: Optional[bool] = False
+    ) -> Dict:
+        """Lists the project of a given user. If the user is omitted, it fallbacks to the currently logged in user"""
+        resp = self._request(
+            "GET",
+            "projects",
+            params={
+                "username": username or "",
+                "include-public": "1" if include_public else "0",
+            },
+        )
+
+        return resp.json()
+
+    def list_files(self, project_id: str) -> List[Dict]:
+        resp = self._request("GET", f"files/{project_id}")
+        return resp.json()
+
+    def download_files(
+        self,
+        project_id: str,
+        local_dir: str,
+        subdir: str = None,
+        continue_on_error: bool = False,
+    ) -> List[Dict]:
+        """Download the specified project files into the destination dir
+
+        Args:
+            project_id: id of the project to be downloaded
+            local_dir: destination directory where the files will be downloaded
+            subdir: if specified, download only files that are withing that subdirectory, otherwise download all
+        """
+
+        files = self.list_files(project_id)
+        files_count = 0
+
+        for file in files:
+            file["status"] = DownloadStatus.PENDING
+            file["status_reason"] = ""
+
+        for file in files:
+            local_file = Path(f'{local_dir}/{file["name"]}')
+            resp = None
+
+            if subdir and not file["name"].startswith(subdir):
+                continue
+
+            try:
+                resp = self._request(
+                    "GET",
+                    f'files/{project_id}/{file["name"]}',
+                    stream=True,
+                )
+                file["status"] = DownloadStatus.SUCCESS
+            except Exception as err:
+                assert resp
+
+                logging.info(
+                    f"{resp.request.method} {resp.url} got HTTP {resp.status_code}"
+                )
+
+                file["status"] = DownloadStatus.FAILED
+                file["status_reason"] = {"status_code": resp.status_code}
+
+                if continue_on_error:
+                    continue
+                else:
+                    raise err
+
+            if not local_file.parent.exists():
+                local_file.parent.mkdir(parents=True)
+
+            with open(local_file, "wb") as f:
+                for chunk in resp.iter_content(chunk_size=8192):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+            files_count += 1
+
+        return files
+
+    def _request(
+        self,
+        method: str,
+        path: str,
+        data: Any = None,
+        params: Dict[str, str] = {},
+        headers: Dict[str, str] = {},
+        files: Dict[str, str] = None,
+        stream: bool = False,
+    ) -> requests.Response:
+        method = method.upper()
+        headers_copy = {**headers}
+
+        if self.token:
+            headers_copy["Authorization"] = f"token {self.token}"
+
+        if path.startswith("/"):
+            path = path[1:]
+
+        if not path.endswith("/"):
+            path += "/"
+
+        response = requests.request(
+            method=method,
+            url=self.url + path,
+            data=data,
+            params=params,
+            headers=headers_copy,
+            files=files,
+            stream=stream,
+            # redirects from POST requests automagically turn into GET requests, so better forbid redirects
+            allow_redirects=(method != "POST"),
+        )
+
+        response.raise_for_status()
+
+        return response
